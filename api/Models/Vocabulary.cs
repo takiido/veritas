5f72929d5ffb0cc5a9406033ebf32ed9e0d2366c
takiido <157808651+takiido@users.noreply.github.com>
2024-09-23 10:35:08 -0500

    created vocabulary model

---
+++ api/Models/Vocabulary.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models
+{
+    public class Vocabulary
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser User { get; set; }
+        public int WordId { get; set; }
+        [ForeignKey("WordId")]
+        public virtual Word Word { get; set; }
+        public string DateAdded {  get; set; }
+        public string DateLastRead { get; set; }
+    }
+}
