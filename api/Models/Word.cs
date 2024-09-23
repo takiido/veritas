5ef806501a1956dee7a51e5eabb10918fffd86f2
takiido <157808651+takiido@users.noreply.github.com>
2024-09-23 10:30:17 -0500

    created word model

---
+++ api/Models/Word.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models
+{
+    public class Word
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+        public string Description { get; set; }
+        public string Example { get; set; }
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+    }
+}
